0925663992e93610572f2a1c2fd4334494d7da20
JoostK <joost.koehoorn@gmail.com>
2020-07-21 12:24:28 +0200

    wip: initial compile of ngDeclareComponent

--- packages/compiler-cli/src/ngtsc/annotations/src/component.ts
+++ packages/compiler-cli/src/ngtsc/annotations/src/component.ts
@@ -337,6 +337,7 @@ export class ComponentDecoratorHandler implements
           ...metadata,
           template: {
             nodes: template.nodes,
+            template: template.template,
             ngContentSelectors: template.ngContentSelectors,
           },
           encapsulation,
@@ -595,13 +596,33 @@ export class ComponentDecoratorHandler implements
   compile(
       node: ClassDeclaration, analysis: Readonly<ComponentAnalysisData>,
       resolution: Readonly<ComponentResolutionData>, pool: ConstantPool): CompileResult[] {
-    const meta: R3ComponentMetadata = {...analysis.meta, ...resolution};
-
-    // FIXME: Consider introducing `compilePrelink`
+    // FIXME: Consider calling `compilePrelink` from the trait compiler
     if (this.compilationModel === CompilationModel.Prelink) {
-      compileDeclareComponentFromMetadata(meta, pool, makeBindingParser());
+      return this.compilePrelink(node, analysis, resolution, pool);
     }
+
+    const meta: R3ComponentMetadata = {...analysis.meta, ...resolution};
     const res = compileComponentFromMetadata(meta, pool, makeBindingParser());
+    const factoryRes = compileNgFactoryDefField(
+        {...meta, injectFn: Identifiers.directiveInject, target: R3FactoryTarget.Component});
+    if (analysis.metadataStmt !== null) {
+      factoryRes.statements.push(analysis.metadataStmt);
+    }
+    return [
+      factoryRes, {
+        name: 'ɵcmp',
+        initializer: res.expression,
+        statements: [],
+        type: res.type,
+      }
+    ];
+  }
+
+  private compilePrelink(
+      node: ClassDeclaration, analysis: Readonly<ComponentAnalysisData>,
+      resolution: Readonly<ComponentResolutionData>, pool: ConstantPool): CompileResult[] {
+    const meta: R3ComponentMetadata = {...analysis.meta, ...resolution};
+    const res = compileDeclareComponentFromMetadata(meta, pool, makeBindingParser());
     const factoryRes = compileNgFactoryDefField(
         {...meta, injectFn: Identifiers.directiveInject, target: R3FactoryTarget.Component});
     if (analysis.metadataStmt !== null) {
