2cdde2f94770a3171c23114dd6528e5b5635040e
JoostK <joost.koehoorn@gmail.com>
2021-06-26 23:56:24 +0200

    test(core): more async-await perf test scenarios and approaches

--- packages/core/test/render3/perf/await_vs_promise/index.ts
+++ packages/core/test/render3/perf/await_vs_promise/index.ts
@@ -10,67 +10,117 @@ import * as tslib from 'tslib';
 import {createBenchmark} from '../micro_bench';
 
 const promises: Promise<number>[] = [];
-for (let i = 0; i < 10_000; i++) {
+for (let i = 0; i < 1_000; i++) {
   promises.push(Promise.resolve(i));
 }
 
-async function nativeAsyncAwait(n: number): Promise<number> {
-  if (n === 0) {
-    return 1;
-  }
-  const index = await promises[n];
-  const nextIndex = await nativeAsyncAwait(index - 1);
-  return nextIndex;
-}
-
-function* proposedGenerativeAsync(n: number): Iterator<Promise<number>, number, number> {
-  if (n === 0) {
-    return 1;
-  }
-  const index = yield promises[n];
-  const nextIndex = yield zoneRunner(proposedGenerativeAsync(index - 1));
-  return nextIndex;
-}
-
 async function zoneRunner(gen: Iterator<any, any, any>) {
-  let res = gen.next();
-  // let zone = Zone.current;
-  let value = await res.value;
-  while (!res.done) {
-    // res = zone.run(gen.next, gen, [value]);
+  let value: any;
+  let res: any;
+  do {
+    // If this throws it should escape the runner
     res = gen.next(value);
-    value = await res.value;
-  }
+
+    try {
+      value = await res.value;
+    } catch (e) {
+      // If this throws it should escape the runner
+      res = gen.throw !(e);
+    }
+  } while (!res.done);
   return value;
 }
 
-function tslibEmulatedGenerator(this: any, n: number) {
-  return tslib.__awaiter(this, void 0, void 0 as any, function(this: any) {
-    var index, nextIndex;
-    return tslib.__generator(this, function(_a: any) {
-      switch (_a.label) {
-        case 0:
-          if (n === 0) {
-            return [2 /*return*/, 1];
-          }
-          return [4 /*yield*/, promises[n]];
-        case 1:
-          index = _a.sent();
-          return [4 /*yield*/, tslibEmulatedGenerator(index - 1)];
-        case 2:
-          nextIndex = _a.sent();
-          return [2 /*return*/, nextIndex];
-      }
-    });
-  });
+async function zoneRunnerWrapper(ctx: any, gen: () => Iterator<any, any, any>) {
+  return zoneRunner(gen.call(ctx));
 }
 
-const benchmarkRefresh = createBenchmark('async');
-const nativeAsyncAwaitTime = benchmarkRefresh('nativeAsyncAwait');
-const proposedGeneratorRunnerTime = benchmarkRefresh('proposedGeneratorRunner');
-const tslibEmulatedGeneratorTime = benchmarkRefresh('tslibEmulatedGenerator');
+// ----------------------------------------------------------------------------
+// Deeply recursive
+// ----------------------------------------------------------------------------
 
-(async () => {
+const recursiveTest = (async () => {
+  async function nativeAsyncAwait(n: number): Promise<number> {
+    if (n === 0) {
+      return 1;
+    }
+    const index = await promises[n];
+    return nativeAsyncAwait(index - 1);
+  }
+
+  function*
+      proposedGenerativeAsync(n: number):
+          Iterator<Promise<number>, number|Promise<number>, number> {
+    if (n === 0) {
+      return 1;
+    }
+    const index = yield promises[n];
+    return zoneRunner(proposedGenerativeAsync(index - 1));
+  }
+
+  function proposedGenerativeAsyncInside(this: any, n: number): any {
+    return zoneRunnerWrapper(
+        this, function*(): Iterator<Promise<number>, number|Promise<number>, number> {
+          if (n === 0) {
+            return 1;
+          }
+          const index = yield promises[n];
+          return proposedGenerativeAsyncInside(index - 1);
+        });
+  }
+
+  function*
+      __proposedGenerativeAsyncContained(n: number):
+          Iterator<Promise<number>, number|Promise<number>, number> {
+    if (n === 0) {
+      return 1;
+    }
+    const index = yield promises[n];
+    return proposedGenerativeAsyncContained(index - 1);
+  }
+
+  function proposedGenerativeAsyncContained(this: any, n: number): Promise<number> {
+    return zoneRunner(__proposedGenerativeAsyncContained.apply(this, arguments as any));
+  }
+
+  function tslibEs2015EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(
+        this, void 0, void 0 as any,
+        function*(this: any): Iterator<Promise<number>, number|Promise<number>, number> {
+          if (n === 0) {
+            return 1;
+          }
+          const index = yield promises[n];
+          return tslibEs2015EmulatedGenerator(index - 1);
+        });
+  }
+
+  function tslibEs5EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(this, void 0, void 0 as any, function(this: any) {
+      var index;
+      return tslib.__generator(this, function(_a: any) {
+        switch (_a.label) {
+          case 0:
+            if (n === 0) {
+              return [2 /*return*/, 1];
+            }
+            return [4 /*yield*/, promises[n]];
+          case 1:
+            index = _a.sent();
+            return [2 /*return*/, tslibEs5EmulatedGenerator(index - 1)];
+        }
+      });
+    });
+  }
+
+  const benchmark = createBenchmark('recursive');
+  const nativeAsyncAwaitTime = benchmark('nativeAsyncAwait');
+  const proposedGeneratorRunnerTime = benchmark('proposedGeneratorRunner');
+  const proposedGeneratorRunnerTimeInside = benchmark('proposedGeneratorRunnerInside');
+  const proposedGeneratorRunnerTimeContained = benchmark('proposedGeneratorRunnerContained');
+  const tslibEs2015EmulatedGeneratorTime = benchmark('tslibEs2015EmulatedGenerator');
+  const tslibEs5EmulatedGeneratorTime = benchmark('tslibEs5EmulatedGenerator');
+
   while (nativeAsyncAwaitTime()) {
     await nativeAsyncAwait(promises.length - 1);
   }
@@ -79,9 +129,215 @@ const tslibEmulatedGeneratorTime = benchmarkRefresh('tslibEmulatedGenerator');
     await zoneRunner(proposedGenerativeAsync(promises.length - 1));
   }
 
-  while (tslibEmulatedGeneratorTime()) {
-    await tslibEmulatedGenerator(promises.length - 1);
+  while (proposedGeneratorRunnerTimeInside()) {
+    await proposedGenerativeAsyncInside(promises.length - 1);
   }
 
-  benchmarkRefresh.report();
+  while (proposedGeneratorRunnerTimeContained()) {
+    await proposedGenerativeAsyncContained(promises.length - 1);
+  }
+
+  while (tslibEs2015EmulatedGeneratorTime()) {
+    await tslibEs2015EmulatedGenerator(promises.length - 1);
+  }
+
+  while (tslibEs5EmulatedGeneratorTime()) {
+    await tslibEs5EmulatedGenerator(promises.length - 1);
+  }
+
+  benchmark.report();
+  console.log('-'.repeat(80));
+})();
+
+
+// ----------------------------------------------------------------------------
+// Iterative test
+// ----------------------------------------------------------------------------
+
+const iterativeTest = (async () => {
+  await recursiveTest;
+
+  async function nativeAsyncAwait(n: number): Promise<number> {
+    while (n !== 0) {
+      const index = await promises[n];
+      n = index - 1;
+    }
+    return n;
+  }
+
+  function* proposedGenerativeAsync(n: number): Iterator<Promise<number>, number, number> {
+    while (n !== 0) {
+      const index = yield promises[n];
+      n = index - 1;
+    }
+    return n;
+  }
+
+  function proposedGenerativeAsyncInside(this: any, n: number): any {
+    return zoneRunnerWrapper(this, function*(): Iterator<Promise<number>, number, number> {
+      while (n !== 0) {
+        const index = yield promises[n];
+        n = index - 1;
+      }
+      return n;
+    });
+  }
+
+  function proposedGenerativeAsyncContained(this: any, n: number): Promise<number> {
+    return zoneRunner(proposedGenerativeAsync.apply(this, arguments as any));
+  }
+
+  function tslibEs2015EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(
+        this, void 0, void 0 as any,
+        function*(this: any): Iterator<Promise<number>, number, number> {
+          while (n !== 0) {
+            const index = yield promises[n];
+            n = index - 1;
+          }
+          return n;
+        });
+  }
+
+  function tslibEs5EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(this, void 0, void 0 as any, function(this: any) {
+      var index;
+      return tslib.__generator(this, function(_a: any) {
+        switch (_a.label) {
+          case 0:
+            if (!(n !== 0)) return [3 /*break*/, 2];
+            return [4 /*yield*/, promises[n]];
+          case 1:
+            index = _a.sent();
+            n = index - 1;
+            return [3 /*break*/, 0];
+          case 2:
+            return [2 /*return*/, n];
+        }
+      });
+    });
+  }
+
+  const benchmark = createBenchmark('iterative');
+  const nativeAsyncAwaitTime = benchmark('nativeAsyncAwait');
+  const proposedGeneratorRunnerTime = benchmark('proposedGeneratorRunner');
+  const proposedGeneratorRunnerTimeInside = benchmark('proposedGeneratorRunnerInside');
+  const proposedGeneratorRunnerTimeContained = benchmark('proposedGeneratorRunnerContained');
+  const tslibEs2015EmulatedGeneratorTime = benchmark('tslibEs2015EmulatedGenerator');
+  const tslibEs5EmulatedGeneratorTime = benchmark('tslibEs5EmulatedGenerator');
+
+  while (nativeAsyncAwaitTime()) {
+    await nativeAsyncAwait(promises.length - 1);
+  }
+
+  while (proposedGeneratorRunnerTime()) {
+    await zoneRunner(proposedGenerativeAsync(promises.length - 1));
+  }
+
+  while (proposedGeneratorRunnerTimeInside()) {
+    await proposedGenerativeAsyncInside(promises.length - 1);
+  }
+
+  while (proposedGeneratorRunnerTimeContained()) {
+    await proposedGenerativeAsyncContained(promises.length - 1);
+  }
+
+  while (tslibEs2015EmulatedGeneratorTime()) {
+    await tslibEs2015EmulatedGenerator(promises.length - 1);
+  }
+
+  while (tslibEs5EmulatedGeneratorTime()) {
+    await tslibEs5EmulatedGenerator(promises.length - 1);
+  }
+
+  benchmark.report();
+  console.log('-'.repeat(80));
+})();
+
+// ----------------------------------------------------------------------------
+// Single await point
+// ----------------------------------------------------------------------------
+
+(async () => {
+  await iterativeTest;
+
+  async function nativeAsyncAwait(n: number): Promise<number> {
+    const index = await promises[n];
+    return index + 1;
+  }
+
+  function* proposedGenerativeAsync(n: number): Iterator<Promise<number>, number, number> {
+    const index = yield promises[n];
+    return index + 1;
+  }
+
+  function proposedGenerativeAsyncInside(this: any, n: number): any {
+    return zoneRunnerWrapper(this, function*(): Iterator<Promise<number>, number, number> {
+      const index = yield promises[n];
+      return index + 1;
+    });
+  }
+
+  function proposedGenerativeAsyncContained(this: any, n: number): Promise<number> {
+    return zoneRunner(proposedGenerativeAsync.apply(this, arguments as any));
+  }
+
+  function tslibEs2015EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(
+        this, void 0, void 0 as any,
+        function*(this: any): Iterator<Promise<number>, number, number> {
+          const index = yield promises[n];
+          return index + 1;
+        });
+  }
+
+  function tslibEs5EmulatedGenerator(this: any, n: number) {
+    return tslib.__awaiter(this, void 0, void 0 as any, function(this: any) {
+      var index;
+      return tslib.__generator(this, function(_a: any) {
+        switch (_a.label) {
+          case 0:
+            return [4 /*yield*/, promises[n]];
+          case 1:
+            index = _a.sent();
+            return [2 /*return*/, index + 1];
+        }
+      });
+    });
+  }
+
+  const benchmark = createBenchmark('once');
+  const nativeAsyncAwaitTime = benchmark('nativeAsyncAwait');
+  const proposedGeneratorRunnerTime = benchmark('proposedGeneratorRunner');
+  const proposedGeneratorRunnerTimeInside = benchmark('proposedGeneratorRunnerInside');
+  const proposedGeneratorRunnerTimeContained = benchmark('proposedGeneratorRunnerContained');
+  const tslibEs2015EmulatedGeneratorTime = benchmark('tslibEs2015EmulatedGenerator');
+  const tslibEs5EmulatedGeneratorTime = benchmark('tslibEs5EmulatedGenerator');
+
+  while (nativeAsyncAwaitTime()) {
+    await nativeAsyncAwait(promises.length - 1);
+  }
+
+  while (proposedGeneratorRunnerTime()) {
+    await zoneRunner(proposedGenerativeAsync(promises.length - 1));
+  }
+
+  while (proposedGeneratorRunnerTimeInside()) {
+    await proposedGenerativeAsyncInside(promises.length - 1);
+  }
+
+  while (proposedGeneratorRunnerTimeContained()) {
+    await proposedGenerativeAsyncContained(promises.length - 1);
+  }
+
+  while (tslibEs2015EmulatedGeneratorTime()) {
+    await tslibEs2015EmulatedGenerator(promises.length - 1);
+  }
+
+  while (tslibEs5EmulatedGeneratorTime()) {
+    await tslibEs5EmulatedGenerator(promises.length - 1);
+  }
+
+  benchmark.report();
+  console.log('-'.repeat(80));
 })();
